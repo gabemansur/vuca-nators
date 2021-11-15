25ee41425ab5ec00b4715fbf02430ea056540cc1
Gabe Mansur <gabe.mansur@gmail.com>
2021-11-15 12:11:44 -0500

    Add basic tests

--- fortuneteller/app/tests.py
+++ fortuneteller/app/tests.py
@@ -1,3 +1,22 @@
 from django.test import TestCase
+from .models import User
 
 # Create your tests here.
+class TestCalls(TestCase):
+    def test_login(self):
+        response = self.client.get('/login')
+        self.assertEqual(response.status_code, 200)
+
+    def test_register(self):
+        response = self.client.get('/register')
+        self.assertEqual(response.status_code, 200)
+
+    def test_user_create(self):
+        self.user = User.objects.create_user(username='testuser', password='12345')
+        self.assertEqual(self.user.username, 'testuser')
+
+    def test_user_login(self):
+        self.user = User.objects.create_user(username='testuser', password='12345')
+        response = self.client.post('/login', {'username': 'testuser', 'password': '12345'}, follow=True)
+        print(response.context['user'])
+        self.assertTrue(response.context['user'].is_authenticated)
