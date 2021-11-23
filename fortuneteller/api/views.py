366bdd571295e0ff9c49c824d034725ac3e28558
JamesBurnsBacon <cookingbaconwithfire@gmail.com>
2021-11-23 18:58:05 -0500

    Mob Programing 11.23

--- fortuneteller/api/views.py
+++ fortuneteller/api/views.py
@@ -3,11 +3,6 @@ from django.http import HttpResponse
 import requests
 import time
 import json
-#testing may need to be moved to tests.py to be cleaner
-import unittest
-
-API_KEY = '20jIEsmDnwLuhZJSWzoDxZXmTq1'
-ticker = 'BTC'
 
 # Create your views here.
 def hello(request):
@@ -28,23 +23,5 @@ def getPrice(ticker):
     params = {'a': ticker, 'api_key': API_KEY, 's': data_start_timestamp, 'u': data_end, 'i': data_frequency}
     response = requests.get(url, params).json()
 
-    return HttpResponse(response[-1]['v'] - response[-2]['v'])
-
-
-#testing may need to be moved to tests.py to be cleaner
-class MyTestCase(unittest.TestCase):
-    def test_default(self):
-        self.assertEqual(True, True)  # add assertion here
-
-    def test_ticker(self):
-        expected = 'BTC'
-        actual = ticker
-        self.assertEqual(expected, actual)
-
-    def test_get_time_now(self):
-        expected = round(time.time())
-        actual = int(round(time.time()))
-        self.assertEqual(expected, actual)
-
-if __name__ == '__main__':
-    unittest.main()
+    #return HttpResponse(round(response[-1]['v'] - response[-2]['v']), 2)
+    return print(round((response[-1]['v'] - response[-2]['v'])/response[-1]['v'])/100, 2)
