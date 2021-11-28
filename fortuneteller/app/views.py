5ec2b9e5c5e89e36ab8b80cae7597c75d39ab5c5
Rachel Wallace <r.wallacer12@gmail.com>
2021-11-28 12:58:36 -0800

    adding workflow_run to catch errors from deploying to production

--- fortuneteller/app/views.py
+++ fortuneteller/app/views.py
@@ -1,12 +1,12 @@
 import json
-#import requests
-#from django.contrib.auth import authenticate, login, logout
-#from django.contrib.auth.models import User
-#from django.db import IntegrityError
-#from django.http import HttpResponseRedirect, response
-#from django.shortcuts import render
-#from django.urls import reverse
-#from .models import User
+import requests
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
+from django.db import IntegrityError
+from django.http import HttpResponseRedirect, response
+from django.shortcuts import render
+from django.urls import reverse
+from .models import User
 
 def index(request):
     return render(request, "app/index.html", {})
